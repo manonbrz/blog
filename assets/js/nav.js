94b365ef9c0fa47f70835f09a77d25d29995c57c
Manon LE BARAZER <manonlebarazer@icloud.com>
2024-07-01 19:11:40 +0200

    start change page

---
+++ assets/js/nav.js
@@ -0,0 +1,15 @@
+// assets/js/nav.js
+document.addEventListener("DOMContentLoaded", function () {
+    let open = document.querySelector(".navbar--icon");
+    let menu = document.querySelector(".nav--open");
+    let close = document.querySelector(".nav--open-icon");
+  
+    open.addEventListener("click", function () {
+      menu.classList.toggle("close");
+    });
+  
+    close.addEventListener("click", function () {
+      menu.classList.toggle("close");
+    });
+  });
+  
