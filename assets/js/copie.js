443fe6e67fb5c859f775d6a16f29b971cdce61c0
Manon LE BARAZER <manonlebarazer@icloud.com>
2024-07-29 16:05:18 +0200

    page entreprise ok

---
+++ assets/js/copie.js
@@ -0,0 +1,10 @@
+function copyCode() {
+    const text = "04 78 17 61 76";
+    const textArea = document.createElement("textarea");
+    textArea.value = text;
+    document.body.appendChild(textArea);
+    textArea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textArea);
+    alert("Numéro copié !");
+}
