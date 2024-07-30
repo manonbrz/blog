10f1b4f7308b67a91536257c187cddf02b281236
Manon LE BARAZER <manonlebarazer@icloud.com>
2024-07-30 11:52:54 +0200

    test deploy

--- assets/php/contact.php
+++ assets/php/contact.php
@@ -4,22 +4,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $email = htmlspecialchars($_POST['email']);
   $message = htmlspecialchars($_POST['message']);
 
-  if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    header("Location: contact.html?error=invalid_email");
-    exit;
-  }
-
   $to = "manonlebarazer@icloud.com";
-  $subject = "Nouveau message de contact de $name";
-  $body = "Nom: $name\nEmail: $email\n\nMessage:\n$message";
+  $subject = "New message from $name";
+  $body = "Name: $name\nEmail: $email\n\nMessage:\n$message";
   $headers = "From: $email";
 
   if (mail($to, $subject, $body, $headers)) {
-    header("Location: contact.html?success=true");
+    echo "Message sent successfully!";
   } else {
-    header("Location: contact.html?error=email_failed");
+    echo "Failed to send the message.";
   }
 } else {
-  header("Location: contact.html?error=invalid_request");
+  echo "Invalid request.";
 }
-?>
