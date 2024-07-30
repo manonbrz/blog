ad76cde7e80905826566289ff15cd2f2652684d9
Manon LE BARAZER <manonlebarazer@icloud.com>
2024-07-30 11:57:37 +0200

    test deploy

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-  fetch('nav.html')
+  fetch('/assets/pages/nav.html')
     .then(response => response.text())
     .then(data => {
       document.getElementById('nav-placeholder').innerHTML = data;
