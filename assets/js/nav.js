75ee6151a7ab8a25b0bc18a597862d4afd7e3010
Manon LE BARAZER <manonlebarazer@icloud.com>
2024-07-31 15:13:55 +0200

    bugfix hebergement

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,9 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-  fetch('../pages/nav.html')
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById('nav-placeholder').innerHTML = data;
-
+ 
       let open = document.querySelector(".navbar--icon");
       let menu = document.querySelector(".nav--open");
       let close = document.querySelector(".nav--open-icon");
@@ -15,5 +11,5 @@ document.addEventListener("DOMContentLoaded", function () {
       close.addEventListener("click", function () {
         menu.classList.toggle("close");
       });
-    });
+    ;
 });
