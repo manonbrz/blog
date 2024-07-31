5c53431fc63dd6cfdc4f2564b22efa2b772ff64b
Manon LE BARAZER <manonlebarazer@icloud.com>
2024-07-31 15:30:30 +0200

    bugfix hebergement

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,15 +1,34 @@
 document.addEventListener("DOMContentLoaded", function () {
- 
-      let open = document.querySelector(".navbar--icon");
-      let menu = document.querySelector(".nav--open");
-      let close = document.querySelector(".nav--open-icon");
+  const homepagePath = window.location.pathname === '/' ? '' : '../';
+  const pathToPages = window.location.pathname.includes('/pages/') ? '' : 'pages/';
 
-      open.addEventListener("click", function () {
-        menu.classList.toggle("close");
-      });
+  const navHtml = `<div class="navbar--container sticky">
+    <div class="navbar--title"><h4>LE BARAZER MANON</h4></div>
+    <div class="navbar--icon"><i class="fas fa-bars"></i></div>
+  </div>
+  <div class="nav--open close sticky">
+    <div class="nav--open-icon"><i class="fas fa-times"></i></div>
+    <div class="nav--open-title">le barazer manon</div>
+    <div class="nav--open-menu">
+      <a href="${homepagePath}index.html">home</a>
+      <a href="${pathToPages}propos.html">à propos</a>
+      <a href="${pathToPages}article.html">articles</a>
+      <a href="${pathToPages}entreprise.html">entreprise</a>
+      <a href="${pathToPages}contact.html">contact</a>
+    </div>
+  </div>`;
 
-      close.addEventListener("click", function () {
-        menu.classList.toggle("close");
-      });
-    ;
+  document.getElementById('nav-placeholder').innerHTML = navHtml;
+
+  let open = document.querySelector(".navbar--icon");
+  let menu = document.querySelector(".nav--open");
+  let close = document.querySelector(".nav--open-icon");
+
+  open.addEventListener("click", function () {
+    menu.classList.toggle("close");
+  });
+
+  close.addEventListener("click", function () {
+    menu.classList.toggle("close");
+  });
 });
