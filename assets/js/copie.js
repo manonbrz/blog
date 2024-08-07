f24acaf4f851ff83061332cf298efedddc64ddc7
Manon LE BARAZER <manonlebarazer@icloud.com>
2024-08-07 11:26:38 +0200

    change copie

--- assets/js/copie.js
+++ assets/js/copie.js
@@ -1,4 +1,4 @@
-function copyCode() {
+function copyCodeGL() {
     const text = "04 78 17 61 76";
     const textArea = document.createElement("textarea");
     textArea.value = text;
@@ -6,5 +6,27 @@ function copyCode() {
     textArea.select();
     document.execCommand("copy");
     document.body.removeChild(textArea);
-    alert("Valeur copié !");
+    alert("Numéro copié !");
+}
+
+function copyCode() {
+    const text = "06 51 82 33 66";
+    const textArea = document.createElement("textarea");
+    textArea.value = text;
+    document.body.appendChild(textArea);
+    textArea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textArea);
+    alert("Numéro copié !");
+}
+
+function copyCodeMail() {
+    const text = "manonlebarazer@icloud.com";
+    const textArea = document.createElement("textarea");
+    textArea.value = text;
+    document.body.appendChild(textArea);
+    textArea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textArea);
+    alert("Mail copié !");
 }
